1a4df5eb4fdd09f7831f8330d22f84bd70614b68
TyRoXx <tyroxxxx@gmail.com>
2012-06-15 17:22:59 +0200

    more tree map members

--- c_containers/tree_map.c
+++ c_containers/tree_map.c
@@ -2,10 +2,18 @@
 #include <assert.h>
 
 
-void tree_map_create(tree_map *map, size_t value_size)
+void tree_map_create(
+	tree_map *map,
+	size_t key_size,
+	size_t value_size,
+	tree_key_comparator_t comparator,
+	void *user_data)
 {
-	map->root = 0;
+	map->key_size = key_size;
 	map->value_size = value_size;
+	map->root = 0;
+	map->comparator = comparator;
+	map->user_data = user_data;
 }
 
 void tree_map_destroy(tree_map *map)
--- c_containers/tree_map.h
+++ c_containers/tree_map.h
@@ -7,15 +7,25 @@
 
 typedef struct tree_node tree_node;
 
+typedef int (*tree_key_comparator_t)(const void *left, const void *right, void *user_data);
+
 typedef struct tree_map
 {
+	size_t key_size;
 	size_t value_size;
 	tree_node *root;
+	tree_key_comparator_t comparator;
+	void *user_data;
 }
 tree_map;
 
 
-void tree_map_create(tree_map *map, size_t value_size);
+void tree_map_create(
+	tree_map *map,
+	size_t key_size,
+	size_t value_size,
+	tree_key_comparator_t comparator,
+	void *user_data);
 void tree_map_destroy(tree_map *map);
 
 
