fe85b2ca74f4d7834f4fac79f46ee4c0fcc896c9
TyRoXx <tyroxxxx@gmail.com>
2012-07-14 01:41:18 +0200

    buffer class

--- webserver/http_request.c
+++ webserver/http_request.c
@@ -2,18 +2,72 @@
 #include <stdlib.h>
 
 
+static void destroy_header(http_header_t *header)
+{
+	free(header->key);
+	free(header->value);
+}
+
+typedef struct buffer_t
+{
+	char *data;
+	size_t size;
+	size_t capacity;
+}
+buffer_t;
+
+static void buffer_create(buffer_t *b)
+{
+	b->data = 0;
+	b->size = b->capacity = 0;
+}
+
+static void buffer_destroy(buffer_t *b)
+{
+	free(b->data);
+}
+
+static bool buffer_reserve(buffer_t *b, size_t capacity)
+{
+	char *new;
+
+	if (capacity <= b->capacity)
+	{
+		return 1;
+	}
+
+	if (capacity < (b->capacity * 2))
+	{
+		capacity = (b->capacity * 2);
+	}
+
+	new = realloc(b->data, capacity);
+	if (new)
+	{
+		b->capacity = capacity;
+		return true;
+	}
+
+	return false;
+}
+
+static bool buffer_resize(buffer_t *b, size_t size)
+{
+	if (!buffer_reserve(b, size))
+	{
+		return false;
+	}
+
+	b->size = size;
+	return true;
+}
+
 bool http_request_parse(
 	http_request_t *request,
 	int (*read)(void *),
 	void *data)
 {
 	return false;
-}
-
-static void destroy_header(http_header_t *header)
-{
-	free(header->key);
-	free(header->value);
 }
 
 void http_request_destroy(
