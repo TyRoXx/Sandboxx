235e4e8a62c90d93b2d224bed9f400d7a6d49fec
TyRoXx <tyroxxxx@gmail.com>
2012-07-25 02:55:39 +0200

    source moved to src/; testing config added

---
+++ webserver/config/index.lua
@@ -0,0 +1,10 @@
+AddHeader("Server", "TyRoXx WebServer 0.1")
+AddHeader("Content-Type", "text/html")
+
+Echo("<html><head><title>WebServer</title></head><body>")
+
+for i = 0, 9 do
+	Echo(i .. "<br>")
+end
+
+Echo("</body></html>")
