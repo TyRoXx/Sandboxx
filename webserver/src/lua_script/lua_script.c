c26f945ab77244170118bcaf270d1c8ae5fefeba
TyRoXx <tyroxxxx@gmail.com>
2012-09-29 02:26:45 +0200

    lua error handling; lua has std libs base, math, string; special html characters are escaped; separate functions for Text (Write) and HTML (Raw) in Lua

--- webserver/src/lua_script/lua_script.c
+++ webserver/src/lua_script/lua_script.c
@@ -6,6 +6,7 @@
 #include "http/directory.h"
 #include <lua.h>
 #include <lauxlib.h>
+#include <lualib.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
@@ -29,7 +30,71 @@ static int script_get_url(lua_State *L)
 	return 1;
 }
 
-static int script_echo(lua_State *L)
+static bool write_escaped(buffer_t *dest, char c)
+{
+	char *str;
+	int rc;
+
+	/*the extra byte is for sprintf's null termination*/
+	if (!buffer_reserve(dest, dest->size + 6 + 1))
+	{
+		return false;
+	}
+
+	str = dest->data + dest->size;
+
+	/*sprintf for simplicity*/
+	rc = sprintf(str, "&#%u;", (unsigned)c);
+
+	/*sprintf can fail*/
+	if (rc < 0)
+	{
+		return false;
+	}
+
+	dest->size += rc;
+	return true;
+}
+
+static int script_write(lua_State *L)
+{
+	execution_context_t * const execution =
+		lua_touserdata(L, lua_upvalueindex(1));
+	buffer_t * const body = &execution->body;
+	const char *text = lua_tostring(L, -1);
+
+	if (!text)
+	{
+		fprintf(stderr, "Write() must be called with a string\n");
+		return 0;
+	}
+
+	for (; *text != '\0'; ++text)
+	{
+		switch (*text)
+		{
+		case '<':
+		case '>':
+		case '&':
+			if (!write_escaped(body, *text))
+			{
+				/*silence for now*/
+				return 0;
+			}
+
+		default:
+			if (!buffer_push_back(body, *text))
+			{
+				/*silence for now*/
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
+
+static int script_raw(lua_State *L)
 {
 	execution_context_t * const execution =
 		lua_touserdata(L, lua_upvalueindex(1));
@@ -37,7 +102,7 @@ static int script_echo(lua_State *L)
 
 	if (!text)
 	{
-		fprintf(stderr, "Echo() must be called with a string\n");
+		fprintf(stderr, "Raw() must be called with a string\n");
 		return 0;
 	}
 
@@ -46,7 +111,7 @@ static int script_echo(lua_State *L)
 		text,
 		strlen(text)))
 	{
-		fprintf(stderr, "Echo() failed\n");
+		/*nothing for now*/
 	}
 
 	return 0;
@@ -104,6 +169,10 @@ static bool handle_lua_request(
 		return false;
 	}
 
+	luaopen_string(L);
+	luaopen_base(L);
+	luaopen_math(L);
+
 	buffer_create(&execution.headers);
 	buffer_create(&execution.body);
 
@@ -112,8 +181,12 @@ static bool handle_lua_request(
 	lua_setglobal(L, "GetURL");
 
 	lua_pushlightuserdata(L, &execution);
-	lua_pushcclosure(L, script_echo, 1);
-	lua_setglobal(L, "Echo");
+	lua_pushcclosure(L, script_write, 1);
+	lua_setglobal(L, "Write");
+
+	lua_pushlightuserdata(L, &execution);
+	lua_pushcclosure(L, script_raw, 1);
+	lua_setglobal(L, "Raw");
 
 	lua_pushlightuserdata(L, &execution);
 	lua_pushcclosure(L, script_add_header, 1);
@@ -141,12 +214,27 @@ static bool handle_lua_request(
 	}
 	else
 	{
+		static const char Message[] = "Internal server error";
+
 		const char * const error_str = lua_tostring(L, -1);
 		if (error_str)
 		{
 			fprintf(stderr, "Lua error: %s\n", error_str);
 			lua_pop(L, 1);
 		}
+
+		response->status = HttpStatus_InternalServerError;
+		response->body_size = strlen(Message);
+
+		if (!imstream_create(&response->body, Message, response->body_size))
+		{
+			fprintf(stderr, "Error in error handling. Terminating to prevent undefined behaviour\n");
+			exit(1);
+		}
+
+		lua_close(L);
+		buffer_destroy(&execution.headers);
+		return true;
 	}
 
 	move_headers(&execution.headers, response);
