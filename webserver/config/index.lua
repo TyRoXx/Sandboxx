7a44dc2d107e5485285b0e54c339e8dfba581696
TyRoXx <tyroxxxx@gmail.com>
2012-09-29 02:33:21 +0200

    example script updated

--- webserver/config/index.lua
+++ webserver/config/index.lua
@@ -1,10 +1,11 @@
 AddHeader("Server", "TyRoXx WebServer 0.1")
 AddHeader("Content-Type", "text/html")
 
-Echo("<html><head><title>WebServer</title></head><body>")
+Raw("<html><head><title>WebServer</title></head><body>")
 
 for i = 0, 9 do
-	Echo(i .. "<br>")
+	Write(tostring(i))
+	Raw("<br>")
 end
 
-Echo("</body></html>")
+Raw("</body></html>")
