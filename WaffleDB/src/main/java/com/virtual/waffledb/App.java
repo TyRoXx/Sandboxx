edc5388ead556e75d73e23465086067aae76a658
TyRoXx <tyroxxxx@gmail.com>
2012-10-31 14:02:01 +0100

    more column options

--- WaffleDB/src/main/java/com/virtual/waffledb/App.java
+++ WaffleDB/src/main/java/com/virtual/waffledb/App.java
@@ -14,24 +14,36 @@ public class App {
         final Database db = new MemoryDatabase();
         try {
             final Map<String, Column> columns = new HashMap<String, Column>();
-            columns.put("name", new Column(db.getStringType(), 0));
-            columns.put("number", new Column(db.getIntegerType(), 1));
+            columns.put("name", new Column(db.getStringType(), true, false, 0));
+            columns.put("comment", new Column(db.getStringType(), false, false, 1));
+            columns.put("number", new Column(db.getIntegerType(), false, true, 2));
+            columns.put("id", new Column(db.getIntegerType(), true, true, 3));
             db.createTable("lang", new TableDefinition(columns));
             System.out.println("Table created");
 
             db.insert("lang", new Value[]{
-                        new StringValue("Java"), new IntegerValue(123),
-                        new StringValue("C++"), new IntegerValue(456)
+                        new StringValue("Java"), new StringValue(""), new IntegerValue(123), new IntegerValue(0),
+                        new StringValue("C++"), new StringValue(""), new IntegerValue(456), new IntegerValue(1)
                     });
             db.insert("lang", new Value[]{});
             System.out.println("Values inserted");
 
             final SelectQueryBuilder select = db.createQueryBuilder();
             select.setSourceTable(db.getTables().get("lang"));
+            select.pushColumn("id");
+            select.popResultColumn();
             select.pushColumn("name");
             select.popResultColumn();
             select.pushColumn("number");
             select.popResultColumn();
+            select.pushColumn("comment");
+            select.popResultColumn();
+
+            select.pushColumn("id");
+            select.pushInteger(1);
+            select.pushComparison(ComparisonType.Equal);
+            select.popCondition();
+
             System.out.println("Select query created");
 
             final ResultSet results = db.select("lang", select);
@@ -39,9 +51,9 @@ public class App {
             System.out.println(results.getRowCount() + " rows");
 
             for (int i = 0; i < results.elements.length; i += results.rowLength) {
-                System.out.print(results.elements[i]);
-                System.out.print(", ");
-                System.out.print(results.elements[i + 1]);
+                for (int j = 0; j < results.rowLength; ++j) {
+                    System.out.print(results.elements[i + j] + ", ");
+                }
                 System.out.println("");
             }
         } catch (final DatabaseException ex) {
--- WaffleDB/src/main/java/com/virtual/waffledb/Column.java
+++ WaffleDB/src/main/java/com/virtual/waffledb/Column.java
@@ -7,10 +7,14 @@ package com.virtual.waffledb;
 public class Column {
 
     public final ColumnType type;
+    public final boolean isUnique;
+    public final boolean hasFastSearch;
     public final int index;
 
-    public Column(ColumnType type, int index) {
+    public Column(ColumnType type, boolean isUnique, boolean hasFastSearch, int index) {
         this.type = type;
+        this.isUnique = isUnique;
+        this.hasFastSearch = hasFastSearch;
         this.index = index;
     }
 }
