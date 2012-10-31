941cb56cfd7d67f8c55db452827962ae6b0421f1
TyRoXx <tyroxxxx@gmail.com>
2012-10-31 10:42:02 +0100

    WaffleDB started

---
+++ WaffleDB/src/main/java/com/virtual/waffledb/App.java
@@ -0,0 +1,13 @@
+package com.virtual.waffledb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
