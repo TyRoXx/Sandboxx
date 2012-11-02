d7a40e1cf256eb542d204781622a00a6f171aafe
TyRoXx <tyroxxxx@gmail.com>
2012-11-02 22:23:32 +0100

    small game started

---
+++ vier_gewinnt/main.cpp
@@ -0,0 +1,246 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <functional>
+#include <cassert>
+
+namespace vg
+{
+	enum cell_possession
+	{
+		red,
+		green,
+		nobody,
+	};
+
+
+	struct field_state
+	{
+		typedef std::vector<cell_possession> cell_vector;
+
+
+		cell_vector cells;
+		std::size_t width;
+
+
+		field_state(std::size_t width, std::size_t height)
+			: cells(width * height, nobody)
+			, width(width)
+		{
+		}
+
+		size_t get_height() const
+		{
+			return (cells.size() / width);
+		}
+
+		cell_possession get_cell(size_t x, size_t y) const
+		{
+			return cells[x + y * width];
+		}
+
+		void set_cell(size_t x, size_t y, cell_possession cell)
+		{
+			cells[x + y * width] = cell;
+		}
+	};
+
+
+	bool is_winning_turn(
+		const field_state &field,
+		size_t row_length,
+		size_t x,
+		size_t y)
+	{
+		return false;
+	}
+
+	bool exists_turn(
+		const field_state &field)
+	{
+		for (size_t x = 0; x < field.width; ++x)
+		{
+			if (field.get_cell(x, 0) == nobody)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	size_t insert_coin(field_state &field, unsigned column, cell_possession color)
+	{
+		assert(color != nobody);
+
+		const auto height = field.get_height();
+
+		size_t y = 0;
+		for (; y < height; ++y)
+		{
+			if (field.get_cell(column, y) != nobody)
+			{
+				if (y == 0)
+				{
+					throw std::runtime_error("Cannot insert into full column");
+				}
+
+				break;
+			}
+		}
+
+		--y;
+		field.set_cell(column, y, color);
+		return y;
+	}
+
+
+	struct player
+	{
+		typedef std::function<unsigned (const field_state &)> choose_column_t;
+
+
+		std::string name;
+		choose_column_t choose_column;
+
+
+		explicit player(
+			std::string name,
+			choose_column_t choose_column)
+			: name(std::move(name))
+			, choose_column(std::move(choose_column))
+		{
+		}
+	};
+
+
+	player *play(
+		player *two_players,
+		const std::function<void (const field_state &)> &field_changed
+		)
+	{
+		field_state field(7, 6);
+		field_changed(field);
+
+		for (bool turn = false; exists_turn(field); turn = !turn)
+		{
+			player &current = two_players[turn];
+
+			const size_t chosen_column = current.choose_column(field);
+			const size_t coin_y = insert_coin(field, chosen_column, turn ? red : green);
+
+			field_changed(field);
+
+			if (is_winning_turn(field, 4, chosen_column, coin_y))
+			{
+				return &current;
+			}
+		}
+
+		return 0;
+	}
+
+	char get_cell_char(cell_possession cell)
+	{
+		switch (cell)
+		{
+		case red: return 'x';
+		case green: return 'o';
+		default: return ' ';
+		}
+	}
+
+	void render_field(
+		std::ostream &out,
+		const field_state &field)
+	{
+		const auto width = field.width;
+		const auto height = field.get_height();
+		const auto horizontal_bar = std::string(width + 2, '-');
+
+		out << horizontal_bar << '\n';
+
+		for (size_t y = 0; y < height; ++y)
+		{
+			out << '|';
+
+			for (size_t x = 0; x < width; ++x)
+			{
+				out << get_cell_char(field.get_cell(x, y));
+			}
+
+			out << "|\n";
+		}
+
+		out << horizontal_bar << '\n';
+
+		out << ' ';
+		for (size_t x = 0; x < width; ++x)
+		{
+			out << static_cast<char>('0' + (x % 10));
+		}
+		out << '\n';
+	}
+
+	bool check_column_input(const field_state &field, size_t column)
+	{
+		if (field.get_cell(column, 0) != nobody)
+		{
+			std::cout << "Diese Spalte ist schon voll\n";
+			return true;
+		}
+
+		return false;
+	}
+
+	size_t let_player_choose_column(const field_state &field)
+	{
+		size_t column = 0;
+		do
+		{
+			std::cin >> column;
+		}
+		while (check_column_input(field, column));
+		return column;
+	}
+
+	size_t let_computer_choose_column(const field_state &field)
+	{
+		size_t x = 0;
+		for (; x < field.width; ++x)
+		{
+			if (field.get_cell(x, 0) == nobody)
+			{
+				break;
+			}
+		}
+		return x;
+	}
+}
+
+int main()
+{
+	using namespace vg;
+
+	player players[2] =
+	{
+		player("player", let_player_choose_column),
+		player("computer", let_computer_choose_column)
+	};
+
+	auto &out = std::cout;
+
+	player * const winner = play(
+		players,
+		std::bind(render_field, std::ref(out), std::placeholders::_1));
+
+	if (winner)
+	{
+		out << winner->name << " gewinnt\n";
+	}
+	else
+	{
+		out << "unentschieden\n";
+	}
+}
