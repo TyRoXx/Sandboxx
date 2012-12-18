66d60bd17753b7bdefd3b13c866fa4d26d3bac2b
TyRoXx <tyroxxxx@gmail.com>
2012-12-18 23:17:14 +0100

    display a BMP

--- SDLTest/main.c
+++ SDLTest/main.c
@@ -15,12 +15,19 @@ int main(void)
 
 	{
 		SDL_Surface * const screen = SDL_SetVideoMode(Width, Height, 32, SDL_SWSURFACE);
+		SDL_Surface * grass;
 		SDL_Event event;
 		int is_running = 1;
 
 		if (!screen)
 		{
 			fprintf(stderr, "Cannot create screen: %s\n", SDL_GetError());
+			return 1;
+		}
+
+		grass = SDL_LoadBMP("sprites/grass32.bmp");
+		if (!grass)
+		{
 			return 1;
 		}
 
@@ -36,21 +43,36 @@ int main(void)
 
 			if (screen->format->BytesPerPixel == 4)
 			{
+				SDL_Rect dest, source;
+
 				unsigned x, y;
 				for (y = 0; y < Height; ++y)
 				{
 					for (x = 0; x < Width; ++x)
 					{
 						uint32_t pixel = SDL_MapRGB(screen->format,
-													x % 256,
-													y % 256,
-													(x + y) % 256);
+													44,
+													44,
+													(Uint8)((float)y / (float)Height * 256.0f));
 
 						memcpy(((char *)screen->pixels) + ((Width * y) + x) * 4,
 							   &pixel,
 							   4);
 					}
 				}
+
+				dest.x = 128;
+				dest.y = 128;
+			//	dest.w = grass->w;
+			//	dest.h = grass->h;
+
+				source.x = 0;
+				source.y = 0;
+				source.w = grass->w;
+				source.h = grass->h;
+
+				SDL_BlitSurface(grass, &source, screen, &dest);
+
 				SDL_UpdateRect(screen, 0, 0, Width, Height);
 			}
 		}
