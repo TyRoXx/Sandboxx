d45b9510ae2458ecec506169ba399f639ef42a0f
TyRoXx <tyroxxxx@gmail.com>
2014-02-05 23:14:18 +0100

    add speed test for std::map and std::unordered_map

--- vector_vs_list/vector_vs_list.cpp
+++ vector_vs_list/vector_vs_list.cpp
@@ -3,6 +3,9 @@
 #include <cctype>
 #include <cstdint>
 #include <iostream>
+#include <unordered_set>
+#include <set>
+#include <boost/random.hpp>
 #include <boost/lexical_cast.hpp>
 
 namespace vsl
@@ -12,13 +15,6 @@ namespace vsl
 	template <class Container>
 	struct tests
 	{
-		static void run_all(std::size_t n)
-		{
-			push_back(n);
-		}
-
-	private:
-
 		static void push_back(std::size_t n)
 		{
 			Container c;
@@ -38,6 +34,27 @@ namespace vsl
 				throw std::logic_error("Cannot happen");
 			}
 		}
+
+		template <class Generator>
+		static void insert_into_set(std::size_t n, Generator const &generate)
+		{
+			Container c;
+			for (std::size_t i = 0; i < n; ++i)
+			{
+				c.insert(generate());
+			}
+
+			typename Container::value_type sum{};
+			for (auto &&e : c)
+			{
+				sum += e;
+			}
+
+			if (sum < 0)
+			{
+				throw std::logic_error("Cannot happen");
+			}
+		}
 	};
 }
 
@@ -48,16 +65,29 @@ int main(int argc, char **argv)
 		std::size_t n = 100000000;
 		if (argc >= 3)
 		{
-			n = boost::lexical_cast<std::size_t>(argv[2]);
+			n = std::max<std::size_t>(1, boost::lexical_cast<std::size_t>(argv[2]));
 		}
+
+		boost::random::mt19937 prng;
+		boost::random::uniform_int_distribution<vsl::element> dist(0, std::numeric_limits<vsl::element>::max() / n);
+		auto const &&random = [&prng, dist] { return dist(prng); };
+
 		switch (std::toupper(argv[1][0]))
 		{
 		case 'V':
-			vsl::tests<std::vector<vsl::element>>::run_all(n);
+			vsl::tests<std::vector<vsl::element>>::push_back(n);
 			return 0;
 
 		case 'L':
-			vsl::tests<std::list<vsl::element>>::run_all(n);
+			vsl::tests<std::list<vsl::element>>::push_back(n);
+			return 0;
+
+		case 'U':
+			vsl::tests<std::unordered_set<vsl::element>>::insert_into_set(n, random);
+			return 0;
+
+		case 'S':
+			vsl::tests<std::set<vsl::element>>::insert_into_set(n, random);
 			return 0;
 
 		default:
