e28f9a1f8a563439c560473eb722fedf59e02987
TyRoXx <tyroxxxx@gmail.com>
2014-06-06 00:20:06 +0200

    replace regex with match

--- new-language/map.cs
+++ new-language/map.cs
@@ -4,19 +4,18 @@ http = require("http", 1.0)
 
 serve_one = (tcp.client client, filesystem.directory static_files) => {
 	request = http.parse_request(client.incoming)
-	positive_response = regex.match_first(
-		request.method,
-		[
-			(regex.from_literal("GET")) => {
+	maybe_response = match(
+		request.method, [
+			("GET") => {
 				requested_file = static_files.read_file(filesystem.path.from_string(request.path))
-				return match(requested_file, [
+				return match(
+					requested_file, [
 					(filesystem.error) => http.make_response(http.file_not_found),
 					(source(byte) content) => http.deliver_file(http.ok, content)
 				])
 			}
-		]
-	)
-	response = positive_response.value_or(http.make_response(http.method_not_allowed))
+		])
+	response = maybe_response.value_or(http.make_response(http.method_not_allowed))
 	return client.outgoing.write(response)
 }
 
