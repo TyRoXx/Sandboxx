8ada6f7d007d6c5139a19060edd5933c44355eb7
TyRoXx <tyroxxxx@gmail.com>
2014-06-06 19:17:27 +0200

    simplify

--- new-language/map.cs
+++ new-language/map.cs
@@ -4,17 +4,17 @@ http = require("http", 1.0)
 
 serve_one = (tcp.client client, filesystem.directory static_files) => {
 	request = http.parse_request(client.incoming)
-	maybe_response = match(
-		request.method, [
-			("GET") => {
-				requested_file = static_files.read_file(filesystem.path.from_string(request.path))
-				return match(
-					requested_file, [
-					(filesystem.error) => http.make_response(http.file_not_found),
-					(source(byte) content) => http.deliver_file(http.ok, content)
-				])
-			}
-		])
+	methods = {
+		"GET" = () => {
+			requested_file = static_files.read_file(filesystem.path.from_string(request.path))
+			responses = [
+				(filesystem.error) => http.make_response(http.file_not_found),
+				(source(byte) content) => http.deliver_file(http.ok, content)
+			]
+			return match(requested_file, responses)
+		}()
+	}
+	maybe_response = methods.find(request.method)
 	response = maybe_response.value_or(http.make_response(http.method_not_allowed))
 	return client.outgoing.write(response)
 }
