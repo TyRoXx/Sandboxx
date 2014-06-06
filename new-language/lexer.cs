d5c359d76fbe1d515c5730817d054c05c9f44448
TyRoXx <tyroxxxx@gmail.com>
2014-06-06 22:12:10 +0200

    reduce redundancy

--- new-language/lexer.cs
+++ new-language/lexer.cs
@@ -18,18 +18,18 @@ make_lexer = (type source_character)
 	read_token = (range(source_character) source)
 		matched = regex.match(source,
 			map.empty
-				.append(regex.literal("\n"), (range(source_character) position) => make_token(token_kind("newline"), position))
-				.append(regex.literal(" "), (range(source_character) position) => make_token(token_kind("space"), position))
-				.append(regex.literal("\t"), (range(source_character) position) => make_token(token_kind("tab"), position))
-				.append(regex.literal("("), (range(source_character) position) => make_token(token_kind("left_parenthesis"), position))
-				.append(regex.literal(")"), (range(source_character) position) => make_token(token_kind("right_parenthesis"), position))
-				.append(regex.literal("."), (range(source_character) position) => make_token(token_kind("dot"), position))
-				.append(regex.literal(","), (range(source_character) position) => make_token(token_kind("comma"), position))
-				.append(regex.range('a', 'z').both_cases().or('_').minimum(1), (range(source_character) position) => make_token(token_kind("identifier"), position))
-				.append(regex.range('0', '9').minimum(1), (range(source_character) position) => make_token(token_kind("integer"), position))
+				.insert(regex.literal("\n"), token_kind("newline"))
+				.insert(regex.literal(" "), token_kind("space"))
+				.insert(regex.literal("\t"), token_kind("tab"))
+				.insert(regex.literal("("), token_kind("left_parenthesis"))
+				.insert(regex.literal(")"), token_kind("right_parenthesis"))
+				.insert(regex.literal("."), token_kind("dot"))
+				.insert(regex.literal(","), token_kind("comma"))
+				.insert(regex.range('a', 'z').both_cases().or('_').minimum(1), token_kind("identifier"))
+				.insert(regex.range('0', '9').minimum(1), token_kind("integer"))
 		)
 		return map.empty
-			.insert("token", matched.result)
+			.insert("token", make_token(matched.result, matched.position))
 			.insert("rest", matched.rest)
 
 return create_package(
