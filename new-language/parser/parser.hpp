879da268188cf9dd7cae200a30185948c87101f8
TyRoXx <tyroxxxx@gmail.com>
2014-06-08 14:24:11 +0200

    test scanner

--- new-language/parser/parser.hpp
+++ new-language/parser/parser.hpp
@@ -3,6 +3,7 @@
 
 #include <silicium/source.hpp>
 #include <silicium/sink.hpp>
+#include <unordered_map>
 
 namespace nl
 {
@@ -12,6 +13,7 @@ namespace nl
 		comma,
 		left_parenthesis,
 		right_parenthesis,
+		assignment,
 		space,
 		tab,
 		newline,
@@ -48,6 +50,17 @@ namespace nl
 		return is_identifier_head(c) || is_decimal_digit(c);
 	}
 
+	template <class Element>
+	boost::optional<Element> peek(Si::source<Element> &source)
+	{
+		auto const next = source.map_next(1);
+		if (next.empty())
+		{
+			return boost::none;
+		}
+		return next.front();
+	}
+
 	inline boost::optional<token> scan_token(Si::source<char> &input)
 	{
 		auto const first = Si::get(input);
@@ -55,33 +68,105 @@ namespace nl
 		{
 			return token{token_type::end_of_file, 0, ""};
 		}
-		switch (*first)
+		static std::unordered_map<char, nl::token_type> const tokens =
 		{
-		case '.': return token{token_type::dot, 0, std::string(1, *first)};
-		case ',': return token{token_type::comma, 0, std::string(1, *first)};
-		case '(': return token{token_type::left_parenthesis, 0, std::string(1, *first)};
-		case ')': return token{token_type::right_parenthesis, 0, std::string(1, *first)};
-		case ' ': return token{token_type::space, 0, std::string(1, *first)};
-		case '\t': return token{token_type::tab, 0, std::string(1, *first)};
-		case '\n': return token{token_type::newline, 0, std::string(1, *first)};
-		case '"':
+			{'.', nl::token_type::dot},
+			{',', nl::token_type::comma},
+			{'(', nl::token_type::left_parenthesis},
+			{')', nl::token_type::right_parenthesis},
+			{'=', nl::token_type::assignment},
+			{' ', nl::token_type::space},
+			{'\t', nl::token_type::tab},
+			{'\n', nl::token_type::newline}
+		};
+		if (*first == '"')
+		{
+			std::string content;
+			for (;;)
 			{
-				throw std::logic_error("not implemented");
-			}
+				auto const next = Si::get(input);
+				if (*next == '"')
+				{
+					return token{token_type::string, 0, std::move(content)};
+				}
+				switch (*next)
+				{
+				case '\\':
+					{
+						auto const escaped = Si::get(input);
+						if (!escaped)
+						{
+							return boost::none;
+						}
+						switch (*escaped)
+						{
+						case '\\':
+						case '"':
+							content.push_back(*escaped);
+							break;
 
-		default:
-			{
-				if (is_identifier_head(*first))
-				{
-					throw std::logic_error("not implemented");
+						default:
+							return boost::none;
+						}
+						break;
+					}
+
+				default:
+					content.push_back(*next);
+					break;
 				}
-				else if (is_decimal_digit(*first))
-				{
-					throw std::logic_error("not implemented");
-				}
-				return boost::none;
 			}
 		}
+		{
+			auto const single_char_found = tokens.find(*first);
+			if (single_char_found != end(tokens))
+			{
+				return token{single_char_found->second, 0, std::string(1, *first)};
+			}
+		}
+		if (is_identifier_head(*first))
+		{
+			std::string content(1, *first);
+			for (;;)
+			{
+				auto const next = peek(input);
+				if (!next)
+				{
+					break;
+				}
+				if (!is_identifier_middle(*next))
+				{
+					break;
+				}
+				content.push_back(*next);
+				Si::get(input);
+			}
+			if (content == "return")
+			{
+				return token{token_type::return_, 0, std::move(content)};
+			}
+			return token{token_type::identifier, 0, std::move(content)};
+		}
+		else if (is_decimal_digit(*first))
+		{
+			std::string content(1, *first);
+			for (;;)
+			{
+				auto const next = peek(input);
+				if (!next)
+				{
+					break;
+				}
+				if (!is_decimal_digit(*next))
+				{
+					break;
+				}
+				content.push_back(*next);
+				Si::get(input);
+			}
+			return token{token_type::integer, 0, std::move(content)};
+		}
+		return boost::none;
 	}
 }
 
--- new-language/parser/test.cpp
+++ new-language/parser/test.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 #include "parser.hpp"
+#include <unordered_map>
 
 BOOST_AUTO_TEST_CASE(scan_token_end_of_file)
 {
@@ -10,4 +11,52 @@ BOOST_AUTO_TEST_CASE(scan_token_end_of_file)
 	BOOST_CHECK(nl::token_type::end_of_file == scanned->type);
 	BOOST_CHECK_EQUAL("", scanned->content);
 	BOOST_CHECK_EQUAL(0, scanned->position);
+}
+
+BOOST_AUTO_TEST_CASE(scan_token_sequence)
+{
+	std::string const input = ".,()= \t\n\"string\"identifier 123 return";
+	auto const tokens =
+	{
+		nl::token_type::dot,
+		nl::token_type::comma,
+		nl::token_type::left_parenthesis,
+		nl::token_type::right_parenthesis,
+		nl::token_type::assignment,
+		nl::token_type::space,
+		nl::token_type::tab,
+		nl::token_type::newline,
+		nl::token_type::string,
+		nl::token_type::identifier,
+		nl::token_type::space,
+		nl::token_type::integer,
+		nl::token_type::space,
+		nl::token_type::return_,
+		nl::token_type::end_of_file
+	};
+	Si::memory_source<char> source(boost::make_iterator_range(input.data(), input.data() + input.size()));
+	for (auto const expected : tokens)
+	{
+		boost::optional<nl::token> const scanned = nl::scan_token(source);
+		BOOST_REQUIRE(scanned);
+		BOOST_CHECK_EQUAL(static_cast<int>(expected), static_cast<int>(scanned->type));
+	}
+	BOOST_CHECK(!Si::get(source));
+}
+
+BOOST_AUTO_TEST_CASE(scan_token_string)
+{
+	std::string const input = "\"abc123\\\"\\\\\"";
+	Si::memory_source<char> source(boost::make_iterator_range(input.data(), input.data() + input.size()));
+	boost::optional<nl::token> scanned = nl::scan_token(source);
+	BOOST_REQUIRE(scanned);
+	BOOST_CHECK(nl::token_type::string == scanned->type);
+	BOOST_CHECK_EQUAL("abc123\"\\", scanned->content);
+	BOOST_CHECK_EQUAL(0, scanned->position);
+
+	boost::optional<nl::token> eof = nl::scan_token(source);
+	BOOST_REQUIRE(eof);
+	BOOST_CHECK(nl::token_type::end_of_file == eof->type);
+	BOOST_CHECK_EQUAL("", eof->content);
+	BOOST_CHECK_EQUAL(0, eof->position);
 }
