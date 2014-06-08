aa5dcd9a5f07be2bb38dc9ad885425b998cd6531
TyRoXx <tyroxxxx@gmail.com>
2014-06-08 17:34:36 +0200

    generator_source was moved to silicium

--- new-language/parser/test.cpp
+++ new-language/parser/test.cpp
@@ -59,74 +59,11 @@ BOOST_AUTO_TEST_CASE(scan_token_string)
 	BOOST_CHECK_EQUAL("", eof->content);
 }
 
-template <class Element, class Generator>
-struct generator_source : Si::source<Element>
-{
-	explicit generator_source(Generator generate_next)
-		: m_generate_next(std::move(generate_next))
-	{
-	}
-
-	virtual boost::iterator_range<Element const *> map_next(std::size_t size) SILICIUM_OVERRIDE
-	{
-		(void)size;
-		return boost::iterator_range<Element const *>();
-	}
-
-	virtual Element *copy_next(boost::iterator_range<Element *> destination) SILICIUM_OVERRIDE
-	{
-		auto copied = destination.begin();
-		for (; copied != destination.end(); ++copied)
-		{
-			auto generated = m_generate_next();
-			if (!generated)
-			{
-				break;
-			}
-			*copied = std::move(*generated);
-		}
-		return copied;
-	}
-
-	virtual boost::uintmax_t minimum_size() SILICIUM_OVERRIDE
-	{
-		return 0;
-	}
-
-	virtual boost::optional<boost::uintmax_t> maximum_size() SILICIUM_OVERRIDE
-	{
-		return boost::none;
-	}
-
-	virtual std::size_t skip(std::size_t count) SILICIUM_OVERRIDE
-	{
-		std::size_t i = 0;
-		for (; i < count; ++i)
-		{
-			if (!m_generate_next())
-			{
-				break;
-			}
-		}
-		return i;
-	}
-
-private:
-
-	Generator m_generate_next;
-};
-
-template <class Element, class Generator>
-generator_source<Element, typename std::decay<Generator>::type> make_generator_source(Generator &&generate_next)
-{
-	return generator_source<Element, typename std::decay<Generator>::type>{std::forward<Generator>(generate_next)};
-}
-
 template <class TokenizerHandler>
 void with_tokenizer(std::string const &code, TokenizerHandler const &handle)
 {
 	Si::memory_source<char> source(boost::make_iterator_range(code.data(), code.data() + code.size()));
-	auto lexer = make_generator_source<nl::token>([&source]
+	auto lexer = Si::make_generator_source<nl::token>([&source]
 	{
 		auto token = nl::scan_token(source);
 		if (!token)
@@ -157,7 +94,7 @@ BOOST_AUTO_TEST_CASE(ast_lambda)
 			"\treturn 1\n"
 			;
 	Si::memory_source<char> source(boost::make_iterator_range(input.data(), input.data() + input.size()));
-	auto lexer = make_generator_source<nl::token>([&source]
+	auto lexer = Si::make_generator_source<nl::token>([&source]
 	{
 		auto token = nl::scan_token(source);
 		if (!token)
